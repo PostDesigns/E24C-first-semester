f719bdab23dd1d6abca8326b6761395167ae4abc
PostDesigns <madspc88@live.dk>
2024-10-31 09:37:45 +0100

    Test

--- javascript-intro/js/js.js
+++ javascript-intro/js/js.js
@@ -3,3 +3,38 @@
  * purpose: Behaviors
  **/
 console.log('Success: JavaScript running!')
+console.log("Hello World");
+
+/*Her er en kommentar, der skriver mojn*/
+console.log("Mojn Do");
+
+/*Variabler - strenge, tal har ikke anførselstegn rundt om sig*/
+let a = "Mojn Dooooo";
+let b = "23";
+
+/*Tal, js kan regne*/
+console.log(b+b);
+let c = 23;
+
+/*Booleans: true/false*/
+let julemandenEksisterer = true;
+console.log("findes julemanden?" + julemandenEksisterer)
+
+let aftensmad = false;
+
+let vaer_23_booked = false;
+
+/*Kontrolstruktur*/
+
+/*
+if (new Date().getHours() < 18) {
+document.getElementById("demo").innerHTML = "Good day!";
+}
+*/
+
+/* Konkatenering: "klistrer sammen" med + */
+let header = "<h1>"+a+"</h1>";
+console.log(header);
+
+document.getElementById("Hej").innerHTML = header;
+document.getElementById("Hej").style.color = "blue";
